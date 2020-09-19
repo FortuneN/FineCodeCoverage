07a5c3a70c020c54d64f35fcc70ea68c609ed921
FortuneN <fortunengwenya@gmail.com>
2020-09-19 20:36:19 +0200

    Thicker indicator + tooltip

--- FineCodeCoverage/Impl/GlyphFactoryProvider.cs
+++ FineCodeCoverage/Impl/GlyphFactoryProvider.cs
@@ -26,31 +26,62 @@ namespace FineCodeCoverage.Impl
 
 		private UIElement GetGlyph(IWpfTextView textView, IWpfTextViewMargin textViewMargin, IWpfTextViewLine textViewLine, IGlyphTag glyphTag)
 		{
-			var tag = (GlyphTag)glyphTag;
+			if (!(glyphTag is GlyphTag tag))
+			{
+				return null;
+			}
 
 			// brush (color)
 
+			var tooltip = default(string);
 			var brush = tag.CoverageLine.HitCount > 0 ? Brushes.Green : Brushes.Red;
-
-			if (brush == Brushes.Green)
+			
+			if
+			(
+				brush == Brushes.Green && 
+				tag.CoverageLine.LineBranches.Count > 1 && 
+				tag.CoverageLine.LineBranches.Any(x => x.Hits == 0)
+			)
 			{
-				if (tag.CoverageLine.LineBranches.Count > 1)
+				brush = CustomGoldBrush;
+
+				var coveredBranchCount = (decimal)tag.CoverageLine.LineBranches.Count(x => x.Hits > 0);
+				var totalBranchCount = (decimal)tag.CoverageLine.LineBranches.Count();
+				var coveredBranchPercent = coveredBranchCount / totalBranchCount * 100m;
+
+				tooltip = $"{coveredBranchCount:0}/{totalBranchCount:0} ({coveredBranchPercent:0}%) branch coverage : {Pluralize("hit", tag.CoverageLine.HitCount)}";
+			}
+
+			// result
+
+			var result = new Rectangle();
+			result.Width = 3;
+			result.Height = 16;
+			result.Fill = brush;
+			result.ToolTip = tooltip; // not showing, why?
+						
+			// return
+
+			return result;
+		}
+
+		private static string Pluralize(string text, int count)
+		{
+			var result = $"{count} {text}";
+
+			if (count != 1)
+			{
+				if (text.EndsWith("ch"))
 				{
-					if (tag.CoverageLine.LineBranches.Any(x => x.Hits == 0))
-					{
-						brush = CustomGoldBrush; // partial coverage
-					}
+					result += "es";
+				}
+				else
+				{
+					result += "s";
 				}
 			}
 
-			// return
-
-			return new Rectangle
-			{
-				Width = 2,
-				Height = 16,
-				Fill = brush
-			};
+			return result;
 		}
 	}
 }
