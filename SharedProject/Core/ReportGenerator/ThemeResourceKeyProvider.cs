de43405e16869758cbc1b4ce9213528c3b851f60
Tony Hallett <tonyhallett74@gmail.com>
2021-11-16 12:59:23 +0000

    exception handling for ThemeResourceKeyProvider

--- SharedProject/Core/ReportGenerator/ThemeResourceKeyProvider.cs
+++ SharedProject/Core/ReportGenerator/ThemeResourceKeyProvider.cs
@@ -18,39 +18,67 @@ namespace FineCodeCoverage.Engine.ReportGenerator
             var fccExtensionsDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             var fccResourcesDirectory = Path.Combine(fccExtensionsDirectory, "Resources");
             var reportPartsPath = Path.Combine(fccResourcesDirectory, "reportparts.xml");
+            if (!File.Exists(reportPartsPath))
+            {
+                throw new Exception($"Cannot find {reportPartsPath}");
+            }
             root = XElement.Load(reportPartsPath);
         }
+
         public ThemeResourceKey Provide(string reportPart)
         {
-            var matchingElement = root.Elements("ReportPart").First(reportPartElement => reportPartElement.Attribute("Name").Value == reportPart);
+            var matchingElement = root.Elements("ReportPart").FirstOrDefault(reportPartElement => reportPartElement.Attribute("Name").Value == reportPart);
             if (matchingElement != null)
             {
                 // probably should change the attribute name
                 var themeResourceKeyString = matchingElement.Attribute("SelectedThemeColourName").Value;
                 var parts = themeResourceKeyString.Split('.');
-                if (parts.Length == 2 && !String.IsNullOrWhiteSpace(parts[1]))
+                if(parts.Length != 2 || String.IsNullOrWhiteSpace(parts[1]))
                 {
-                    var @class = parts[0];
-                    Type classType = null;
-                    switch (@class)
+                    throw new Exception($"report part {reportPart} not of format ClassName.PropertyName");
+                }
+                
+                
+                var @class = parts[0];
+                Type classType = null;
+                switch (@class)
+                {
+                    case "EnvironmentColors":
+                        classType = typeof(EnvironmentColors);
+                        break;
+                    case "CommonControlsColors":
+                        classType = typeof(CommonControlsColors);
+                        break;
+                }
+                if (classType != null)
+                {
+                    var themeResourceKeyProperty = classType.GetProperty(parts[1], BindingFlags.Public | BindingFlags.Static);
+                    if (themeResourceKeyProperty != null)
                     {
-                        case "EnvironmentColors":
-                            classType = typeof(EnvironmentColors);
-                            break;
-                        case "CommonControlsColors":
-                            classType = typeof(CommonControlsColors);
-                            break;
-                    }
-                    if (classType != null)
-                    {
-                        //try ?
-                        var themeResourceKeyProperty = classType.GetProperty(parts[1], BindingFlags.Public | BindingFlags.Static);
-                        if (themeResourceKeyProperty != null)
+                        var themeResourceKey = themeResourceKeyProperty.GetValue(null);
+                        if(themeResourceKey is ThemeResourceKey)
                         {
-                            return themeResourceKeyProperty.GetValue(null) as ThemeResourceKey;
+                            return themeResourceKey as ThemeResourceKey;
+                        }
+                        else
+                        {
+                            throw new Exception($"report part {reportPart}: property {parts[1]} is not of type ThemeResourceKey");
                         }
                     }
+                    else
+                    {
+                        throw new Exception($"report part {reportPart}: class {@class} does not have property {parts[1]}");
+                    }
                 }
+                else
+                {
+                    throw new Exception($"report part {reportPart} class name should be EnvironmentColors | CommonControlsColors");
+                }
+                
+            }
+            else
+            {
+                throw new Exception($"Cannot find report part {reportPart}");
             }
             return null;
         }
