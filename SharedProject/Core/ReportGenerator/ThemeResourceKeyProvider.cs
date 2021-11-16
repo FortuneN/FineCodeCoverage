41b6332316e5ebbe26020e9a10f13c456ca4d6c7
Tony Hallett <tonyhallett74@gmail.com>
2021-11-16 12:38:48 +0000

    file per type

---
+++ SharedProject/Core/ReportGenerator/ThemeResourceKeyProvider.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.PlatformUI;
+using Microsoft.VisualStudio.Shell;
+using System;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Xml.Linq;
+
+namespace FineCodeCoverage.Engine.ReportGenerator
+{
+    [Export(typeof(IThemeResourceKeyProvider))]
+    internal class ThemeResourceKeyProvider : IThemeResourceKeyProvider
+    {
+        private XElement root;
+        public ThemeResourceKeyProvider()
+        {
+            var fccExtensionsDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var fccResourcesDirectory = Path.Combine(fccExtensionsDirectory, "Resources");
+            var reportPartsPath = Path.Combine(fccResourcesDirectory, "reportparts.xml");
+            root = XElement.Load(reportPartsPath);
+        }
+        public ThemeResourceKey Provide(string reportPart)
+        {
+            var matchingElement = root.Elements("ReportPart").First(reportPartElement => reportPartElement.Attribute("Name").Value == reportPart);
+            if (matchingElement != null)
+            {
+                // probably should change the attribute name
+                var themeResourceKeyString = matchingElement.Attribute("SelectedThemeColourName").Value;
+                var parts = themeResourceKeyString.Split('.');
+                if (parts.Length == 2 && !String.IsNullOrWhiteSpace(parts[1]))
+                {
+                    var @class = parts[0];
+                    Type classType = null;
+                    switch (@class)
+                    {
+                        case "EnvironmentColors":
+                            classType = typeof(EnvironmentColors);
+                            break;
+                        case "CommonControlsColors":
+                            classType = typeof(CommonControlsColors);
+                            break;
+                    }
+                    if (classType != null)
+                    {
+                        //try ?
+                        var themeResourceKeyProperty = classType.GetProperty(parts[1], BindingFlags.Public | BindingFlags.Static);
+                        if (themeResourceKeyProperty != null)
+                        {
+                            return themeResourceKeyProperty.GetValue(null) as ThemeResourceKey;
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+    }
+
+}
