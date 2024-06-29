dce078f19730f7c11c3877b192564e39560ff245
Tony Hallett <tonyhallett74@gmail.com>
2024-06-29 12:10:34 +0100

    remove duplicate coverage lines

--- FineCodeCoverageTests/FileLineCoverage_Tests.cs
+++ FineCodeCoverageTests/FileLineCoverage_Tests.cs
@@ -16,15 +16,16 @@ namespace FineCodeCoverageTests
             return mockLine.Object;
         }
 
-        [TestCaseSource(nameof(Cases))]
-        public void GetLines_Test(IEnumerable<int> lineNumbers, int startLineNumber, int endLineNumber, IEnumerable<int> expectedLineNumbers)
+        [Test]
+        public void Should_Return_Distinct_Sorted_Lines()
         {
             var fileLineCoverage = new FileLineCoverage();
-            fileLineCoverage.Add("fp", lineNumbers.Select((n => CreateLine(n))));
+            fileLineCoverage.Add("file1", new[] { CreateLine(2), CreateLine(1), CreateLine(3),CreateLine(1) });
             fileLineCoverage.Sort();
 
-            var lines = fileLineCoverage.GetLines("fp", startLineNumber, endLineNumber);
-            Assert.That(lines.Select(l => l.Number), Is.EqualTo(expectedLineNumbers));
+            var lines = fileLineCoverage.GetLines("file1");
+            
+            Assert.That(lines.Select(l => l.Number), Is.EqualTo(new int[] { 1,2,3}));
         }
 
         [Test]
@@ -32,7 +33,7 @@ namespace FineCodeCoverageTests
         {
             var fileLineCoverage = new FileLineCoverage();
 
-            var lines = fileLineCoverage.GetLines("", 1, 2);
+            var lines = fileLineCoverage.GetLines("");
 
             Assert.That(lines, Is.Empty);
         }
@@ -50,6 +51,7 @@ namespace FineCodeCoverageTests
             var fileLineCoverage = new FileLineCoverage();
             var lines = new[] { CreateLine(1), CreateLine(2) };
             fileLineCoverage.Add("old", lines);
+            fileLineCoverage.Sort();
             AssertLines("old");
             
             fileLineCoverage.UpdateRenamed("old", "new");
@@ -62,18 +64,5 @@ namespace FineCodeCoverageTests
                 Assert.That(allLines, Is.EqualTo(lines));
             }
         }
-
-        static readonly object[] Cases =
-        {
-            new object[] { new int[] { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}, 19, 20, new int[]{ 19,20} },
-            new object[] { new int[] { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}, 12, 13, new int[]{ 12,13} },
-            new object[] { new int[] { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}, 6, 7, new int[]{ 6,7} },
-            new object[] {Enumerable.Empty<int>(), 0, 4,Enumerable.Empty<int>() },
-            new object[] { new int[] { 3,2,1}, 0, 4, new int[]{ 1,2,3} },
-            new object[] { new int[] { 3,2,1}, 0, 3, new int[]{ 1,2,3} },
-            new object[] { new int[] { 3,2,1}, 1, 2, new int[]{ 1,2} },
-            new object[] { new int[] { 3,2,1}, 2, 2, new int[]{ 2} },
-            new object[] { new int[] { 3,2,1}, 4, 5, Enumerable.Empty<int>() }
-        };
     }
 }
