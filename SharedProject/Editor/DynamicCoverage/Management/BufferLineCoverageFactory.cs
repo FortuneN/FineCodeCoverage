8ceb999c8d0ad9cfddc3892edceb13345c37e034
Tony Hallett <tonyhallett74@gmail.com>
2024-10-17 12:52:23 +0100

    remove tracked lines when file type changes to unsupported content type when supported is open in editor

--- SharedProject/Editor/DynamicCoverage/Management/BufferLineCoverageFactory.cs
+++ SharedProject/Editor/DynamicCoverage/Management/BufferLineCoverageFactory.cs
@@ -1,22 +1,37 @@
 ﻿using System.ComponentModel.Composition;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using FineCodeCoverage.Core.Utilities;
+using FineCodeCoverage.Editor.DynamicCoverage.TrackedLinesImpl.Construction;
 using FineCodeCoverage.Engine.Model;
 using FineCodeCoverage.Impl;
 using FineCodeCoverage.Options;
 
 namespace FineCodeCoverage.Editor.DynamicCoverage
 {
+    internal class CoverageContentTypes : ICoverageContentTypes
+    {
+        private readonly ICoverageContentType[] coverageContentTypes;
+
+        public CoverageContentTypes(ICoverageContentType[] coverageContentTypes) 
+            => this.coverageContentTypes = coverageContentTypes;
+        public bool IsApplicable(string contentTypeName) 
+            => this.coverageContentTypes.Any(contentType => contentType.ContentTypeName == contentTypeName);
+    }
+
     [ExcludeFromCodeCoverage]
     [Export(typeof(IBufferLineCoverageFactory))]
     internal class BufferLineCoverageFactory : IBufferLineCoverageFactory
     {
+        private readonly ICoverageContentTypes coverageContentTypes;
         private readonly IDynamicCoverageStore dynamicCoverageStore;
         private readonly IAppOptionsProvider appOptionsProvider;
         private readonly ILogger logger;
 
         [ImportingConstructor]
         public BufferLineCoverageFactory(
+            [ImportMany]
+            ICoverageContentType[] coverageContentTypes,
             IDynamicCoverageStore dynamicCoverageStore,
             IAppOptionsProvider appOptionsProvider,
             ILogger logger
@@ -24,6 +39,7 @@ namespace FineCodeCoverage.Editor.DynamicCoverage
         {
             this.appOptionsProvider = appOptionsProvider;
             this.logger = logger;
+            this.coverageContentTypes = new CoverageContentTypes(coverageContentTypes);
             this.dynamicCoverageStore = dynamicCoverageStore;
         }
 
@@ -39,7 +55,9 @@ namespace FineCodeCoverage.Editor.DynamicCoverage
                 trackedLinesFactory,
                 this.dynamicCoverageStore,
                 this.appOptionsProvider,
+                this.coverageContentTypes,
                 this.logger
+
                 );
     }
 }
